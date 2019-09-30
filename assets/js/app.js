b64098c91b95f8d785f362edcd692bf7d55fb4d1
Ed Warnicke <hagbard@gmail.com>
2019-09-29 23:06:59 -0500

    Fix to nsmcon schedule menus.
    Signed-off-by: Ed Warnicke <hagbard@gmail.com>

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,7 +14,20 @@ document.addEventListener('DOMContentLoaded', function() {
   let cardToggles = document.getElementsByClassName('card-toggle');
   for (let i = 0; i < cardToggles.length; i++) {
     cardToggles[i].addEventListener('click', e => {
-      e.currentTarget.childNodes[3].classList.toggle('is-hidden');
+      for (let j=0;j< e.currentTarget.childNodes.length;j++) {
+        let n = e.currentTarget.childNodes[j]
+        if (n.nodeType != Node.TEXT_NODE &&  n.classList.contains("card-content")) {
+          n.classList.toggle('is-hidden');
+        }
+      }
     });
+    if (cardToggles[i].id == location.hash.substr(1)) {
+      for (let j=0;j< cardToggles[i].childNodes.length;j++) {
+        let n = cardToggles[i].childNodes[j]
+        if (n.nodeType != Node.TEXT_NODE &&  n.classList.contains("card-content")) {
+          n.classList.toggle('is-hidden');
+        }
+      }
+    }
   }
 });
