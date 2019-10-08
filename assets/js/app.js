4a30c73568be6201c9718351b52ba54c8602c624
Ed Warnicke <hagbard@gmail.com>
2019-10-07 19:43:08 -0500

    Fix collapsibility (#125)
    Right now clicking *anywhere* in the content of an expanded talk collapses the talk.
    This PR makes the collapse happen only if you click on the header.
    
    Signed-off-by: Ed Warnicke <hagbard@gmail.com>

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,17 +14,17 @@ document.addEventListener('DOMContentLoaded', function() {
   let cardToggles = document.getElementsByClassName('card-toggle');
   for (let i = 0; i < cardToggles.length; i++) {
     cardToggles[i].addEventListener('click', e => {
-      for (let j=0;j< e.currentTarget.childNodes.length;j++) {
-        let n = e.currentTarget.childNodes[j]
-        if (n.nodeType != Node.TEXT_NODE &&  n.classList.contains("card-content")) {
+      let n = e.currentTarget
+      while (n=n.nextSibling) {
+        if (n.nodeType != Node.TEXT_NODE && n.classList.contains("card-content")) {
           n.classList.toggle('is-hidden');
         }
       }
     });
     if (cardToggles[i].id == location.hash.substr(1)) {
-      for (let j=0;j< cardToggles[i].childNodes.length;j++) {
-        let n = cardToggles[i].childNodes[j]
-        if (n.nodeType != Node.TEXT_NODE &&  n.classList.contains("card-content")) {
+      let n = cardToggles[i]
+      while (n=n.nextSibling) {
+        if (n.nodeType != Node.TEXT_NODE && n.classList.contains("card-content")) {
           n.classList.toggle('is-hidden');
         }
       }
